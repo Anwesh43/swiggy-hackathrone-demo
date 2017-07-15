a13062d76f2f3891d544806b830418c80b5a135c
Anwesh43 <anweshthecool0@gmail.com>
2017-07-16 04:00:55 +0530

    adding demo file

--- demo_app.py
+++ demo_app.py
@@ -4,29 +4,42 @@ import os
 from speech_recognition import *
 recognizer = Recognizer()
 phno = 'xxab'
+items = ['Restaurant','FoodItem','ItemId']
+greets = ['Here are the top restaurants for you','Here is the menu']
+order = {}
 def start():
     r = requests.get('http://localhost:5000/request-speech/{0}'.format(phno))
     print(r.text)
     n =  int(r.text)
     startLoop(3)
+def speak(text,file_name):
+    tts = gTTS(text=text, lang='hi')
+    tts.save(file_name)
+    os.system("afplay {0}".format(file_name))
 def startLoop(n):
     i = 0
     while i < n:
         r = requests.get('http://localhost:5000/fetchQuestion/{0}/{1}'.format(phno,i))
         text = r.text
         print(text)
-        tts = gTTS(text=text, lang='en')
-        tts.save("resp.mp3")
-        os.system("afplay resp.mp3")
+        speak(text,"resp_{0}_{1}.mp3".format(phno,i))
         with Microphone() as source:
             audio = recognizer.listen(source)
         try:
             text = recognizer.recognize_google(audio)
             print(text)
-            new_r = requests.get('http://localhost:5000/processText/{0}'.format(text))
-            status = new_r.text
-            if status == "success":
+            new_r = requests.get('http://localhost:5000/processText/{1}/{0}'.format(text,i))
+            resp = new_r.json()
+            print(resp)
+            if resp["status"] == "success":
+                print("success")
+                if not(i == n-1):
+                    speak('{0} in {1}'.format(greets[i],text),"greet_{0}.mp3".format(i))
+                    for data in resp["data"]:
+                        print(data[items[i]])
+                        speak(data[items[i]],"{0}.mp3".format(data[items[i]].replace(' ','_')))
                 i = i+1
+
         except:
             print("error")
     if i == n:
