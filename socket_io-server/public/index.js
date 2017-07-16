8e7fbf14cc5ffc36b442eb38affe211d036f6c51
Anwesh43 <anweshthecool0@gmail.com>
2017-07-16 07:37:36 +0530

    notifying the client

--- socket_io-server/public/index.js
+++ socket_io-server/public/index.js
@@ -3,6 +3,6 @@ socket.on('hello',(greet)=> {
     alert(greet)
 })
 socket.on("new_order",(order)=>{
-    document.write(order.name+" "+order.price)
+    document.write(order.place+" "+order.restaurant+" "+order.food_item+" "+order.cost)
     console.log(order)
 })
