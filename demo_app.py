3d8a1942eee8a4db0fa22eda23a2d1edabc47204
Anwesh43 <anweshthecool0@gmail.com>
2017-07-16 06:19:38 +0530

    adding fixes for creating orderJSON

--- demo_app.py
+++ demo_app.py
@@ -1,12 +1,18 @@
 import requests
 from gtts import gTTS
 import os
+import difflib
 from speech_recognition import *
 recognizer = Recognizer()
 phno = 'xxab'
 items = ['Restaurant','FoodItem','ItemId']
 greets = ['Here are the top restaurants for you','Here is the menu']
-order = {}
+orderItems = ['Restaurant_id','ItemId']
+valItems = ['Restaurant_id','values']
+prevItemDictionary = {}
+orderDictionary = {}
+orderJSONItems = ["place","restaurant","food_item","cost"]
+orderJSON = {}
 def start():
     r = requests.get('http://localhost:5000/request-speech/{0}'.format(phno))
     print(r.text)
@@ -32,18 +38,35 @@ def startLoop(n):
             resp = new_r.json()
             print(resp)
             if resp["status"] == "success":
+                key_text = text
+                for key in prevItemDictionary.keys():
+                    if key.lower() in key_text:
+                        key_text = key
+                if key_text in prevItemDictionary:
+                    if i == n-1:
+                        orderDictionary[valItems[i-1]] = [{"item_id":prevItemDictionary[key_text]}]
+                    else:
+                        orderDictionary[valItems[i-1]] = prevItemDictionary[key_text]
                 print("success")
                 if not(i == n-1):
-                    speak('{0} in {1}'.format(greets[i],text),"greet_{0}.mp3".format(i))
+                    speak('{0} in {1}'.format(greets[i],resp["orig_text"]),"greet_{0}.mp3".format(i))
                     for data in resp["data"]:
                         print(data[items[i]])
                         speak(data[items[i]],"{0}.mp3".format(data[items[i]].replace(' ','_')))
+                        prevItemDictionary[data[items[i]]] = data[orderItems[i]]
+                    orderJSON[orderJSONItems[i]] = resp["orig_text"]
+                else:
+                    print(orderDictionary)
+                    cost = float(resp["data"])
+                    orderJSON[orderJSONItems[i]] = cost
                 i = i+1
 
-        except:
+        except Exception as e:
+            print(e)
             print("error")
     if i == n:
-        r = requests.post('http://localhost:9000/postorder',data={"name":"order1","price":100})
+        print(orderJSON)
+        r = requests.post('http://localhost:9000/postorder',data=orderJSON)
         print(r.text)
     #print(type(json))
 if __name__ == "__main__":
